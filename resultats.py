1660313d3542b15ce707831de648712be7cf98d2
Stanis Humez <stanis@netvolution.fr>
2019-07-15 18:32:01 +0200

    Amélioration des données de test

--- resultats.py
+++ resultats.py
@@ -1,5 +1,13 @@
-mescandidats = ['fr', 'it', 'uk', 'es', 'de']
+'''
+Jeu de données test pour les algorithmes d'élections
+'''
 
+# Liste des candidats (chaîne de caractères avec leur nom
+mescandidats = ['François (fr)', 'Marcello (it)', 'John (uk)', 'Esperanza (es)', 'Ute (de)']
+
+# Résultats des votes
+# votes = nombre de millions de voix obtenues
+# classement = ordre d'arrivée des candidats (dans la liste "mescandidats") pour ce nombre de voix
 mesresultats = [
                 {
                     "votes": 7.2, "classement": [0, 1, 2, 3, 4]
@@ -8,7 +16,7 @@ mesresultats = [
                     "votes": 4.8, "classement": [4, 2, 1, 3, 0],
                 },
                 {
-                    "votes": 4, "classement" : [3, 4, 2, 1, 0],
+                    "votes": 4, "classement": [3, 4, 2, 1, 0],
                 },
                 {
                     "votes": 3.6, "classement": [1, 3, 2, 4, 0],
@@ -20,4 +28,3 @@ mesresultats = [
                     "votes": 0.8, "classement": [2, 3, 1, 4, 0]
                 },
 ]
-
